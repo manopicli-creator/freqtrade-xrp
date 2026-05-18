cdd0c1cee984b412f27eadcaed3ec69d94bb186b
Manopic <manopicli@gmail.com>
2026-05-18 18:49:35 +0200

    Auto download data on startup

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+mkdir -p user_data/data user_data/logs user_data/strategies
+
+# Télécharger les données
+freqtrade download-data --pairs XRP/USDT --exchange gateio --timeframe 5m --days 30
+
 # Lance Freqtrade en arrière-plan
-freqtrade trade --strategy XRPStrategy --logfile /app/user_data/logs/freqtrade.log &
+freqtrade trade --strategy XRPStrategy &
 
 # Lance Flask
 python server.py
