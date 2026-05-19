0a3cbd0b6d9f8c3849af715f75e568bd8da18d61
manopicli-creator <manopicli@gmail.com>
2026-05-19 17:33:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,18 @@ echo "=== Starting setup ==="
 mkdir -p user_data/data user_data/logs user_data/strategies
 
 echo "=== Downloading data ==="
-freqtrade download-data --pairs XRP/USDT --exchange gateio --timeframe 5m --days 7
+freqtrade download-data \
+  --pairs XRP/USDT \
+  --exchange kraken \
+  --timeframe 5m \
+  --days 7 || echo "Download failed, continuing anyway..."
 
 echo "=== Starting Freqtrade ==="
-freqtrade trade --strategy XRPStrategy --logfile user_data/logs/freqtrade.log &
+freqtrade trade \
+  --strategy XRPStrategy \
+  --config user_data/config.json \
+  --logfile user_data/logs/freqtrade.log &
+
 FTPID=$!
 echo "Freqtrade PID: $FTPID"
 
