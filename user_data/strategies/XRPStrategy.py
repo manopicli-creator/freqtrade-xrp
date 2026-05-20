f5a46b703eb3ab7d7c6a638bb0e644357dc0f239
manopicli-creator <manopicli@gmail.com>
2026-05-20 22:24:04 +0200

    Update XRPStrategy.py

--- user_data/strategies/XRPStrategy.py
+++ user_data/strategies/XRPStrategy.py
@@ -1,12 +1,9 @@
 from freqtrade.strategy import IStrategy, IntParameter
 from pandas import DataFrame
 import talib.abstract as ta
-
 class XRPStrategy(IStrategy):
-
     INTERFACE_VERSION = 3
     timeframe = '15m'
-
     stoploss = -0.05
     minimal_roi = {
         "0": 0.04,
@@ -14,41 +11,31 @@ class XRPStrategy(IStrategy):
         "240": 0.01,
         "480": 0
     }
-
     trailing_stop = True
     trailing_stop_positive = 0.02
     trailing_stop_positive_offset = 0.03
     trailing_only_offset_is_reached = True
-
     use_exit_signal = False
-
     buy_rsi_min = IntParameter(30, 50, default=38, space='buy')
     buy_rsi_max = IntParameter(50, 70, default=62, space='buy')
-
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         dataframe['ema20'] = ta.EMA(dataframe, timeperiod=20)
         dataframe['ema50'] = ta.EMA(dataframe, timeperiod=50)
         dataframe['ema200'] = ta.EMA(dataframe, timeperiod=200)
-
         macd = ta.MACD(dataframe, fastperiod=12, slowperiod=26, signalperiod=9)
         dataframe['macd'] = macd['macd']
         dataframe['macdsignal'] = macd['macdsignal']
-
         bollinger = ta.BBANDS(dataframe, timeperiod=20, nbdevup=2.0, nbdevdn=2.0)
         dataframe['bb_upper'] = bollinger['upperband']
         dataframe['bb_lower'] = bollinger['lowerband']
-
         dataframe['macd_cross_up'] = (
             (dataframe['macd'] > dataframe['macdsignal']) &
             (dataframe['macd'].shift(1) <= dataframe['macdsignal'].shift(1))
         )
-
         # Filtre tendance macro (équivalent EMA200 sur 1h)
         dataframe['ema200_1h'] = ta.EMA(dataframe, timeperiod=800)
-
         return dataframe
-
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
@@ -63,7 +50,6 @@ class XRPStrategy(IStrategy):
             ),
             'enter_long'] = 1
         return dataframe
-
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[:, 'exit_long'] = 0
         return dataframe
