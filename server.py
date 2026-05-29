12a32460ce444d0a9b22c9318bfa35a1c77a3e7a
manopicli-creator <manopicli@gmail.com>
2026-05-29 12:22:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -287,3 +287,4 @@ if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
+    
